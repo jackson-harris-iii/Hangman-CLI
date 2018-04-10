b3279ef7df90ac103eb451cf85975cc6b49be206
Apollo <jackson.harris3@gmail.com>
2018-04-10 01:40:47 -0700

    word choices array, create random selection function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const iq = require('inquirer')
 var makeWord = require('./word.js')
 
+let prompt = iq.createPromptModule();
+
+const choices = ["JAINA", "REXXAR", "THRALL", "MALFURION", "UTHER", "ANDUIN", "VALEERA", "GULDAN", "GARROSH"];
+
+function random () {
+    console.log(choices[Math.floor(Math.random()*choices.length)])
+}
+
+random()
